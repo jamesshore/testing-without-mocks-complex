572481443842da44e95bcff9b4be26529fa51bc1
James Shore <jshore@jamesshore.com>
2022-03-17 17:53:36 -0700

    Extract Rot13Controller from Rot13Router

--- src/rot13-service/routing/rot13_router.js
+++ src/rot13-service/routing/rot13_router.js
@@ -5,6 +5,7 @@ const ensure = require("util/ensure");
 const rot13Response = require("./rot13_response");
 const rot13 = require("../logic/rot13");
 const HttpRequest = require("http/http_request");
+const Rot13Controller = require("./rot13_controller");
 
 const REQUEST_TYPE = { text: String };
 
@@ -15,18 +16,5 @@ exports.routeAsync = async function(request) {
 	if (request.urlPathname !== "/rot13/transform") return rot13Response.error(404, "not found");
 	if (request.method !== "POST") return rot13Response.error(405, "method not allowed");
 
-	if (!request.hasContentType("application/json")) return rot13Response.badRequest("invalid content-type header");
-	const jsonString = await request.readBodyAsync();
-	let json;
-	try {
-		json = JSON.parse(jsonString);
-		ensure.typeMinimum(json, REQUEST_TYPE, "request");
-	}
-	catch (err) {
-		return rot13Response.badRequest(err.message);
-	}
-
-	const input = json.text;
-	const output = rot13.transform(input);
-	return rot13Response.ok(output);
+	return await (new Rot13Controller()).postAsync(request);
 };
