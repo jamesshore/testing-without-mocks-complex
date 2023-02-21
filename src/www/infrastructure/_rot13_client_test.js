78e3b837152083a4f8c67fffed957b23df1aebe2
James Shore <jshore@jamesshore.com>
2023-02-21 12:30:50 -0800

    Convert TestHelper to ESM

--- src/www/infrastructure/_rot13_client_test.js
+++ src/www/infrastructure/_rot13_client_test.js
@@ -3,7 +3,7 @@ import ensure from "util/ensure.cjs";
 import assert from "util/assert.cjs";
 import { HttpClient } from "http/http_client.mjs";
 import { Rot13Client } from "./rot13_client.js";
-import testHelper from "util/test_helper.cjs";
+import { ignorePromiseErrorAsync } from "util/test_helper.mjs";
 
 const HOST = "localhost";
 const IRRELEVANT_PORT = 42;
@@ -156,7 +156,7 @@ describe("ROT-13 Service client", () => {
 			};
 
 			cancelFn();
-			await testHelper.ignorePromiseErrorAsync(responsePromise);
+			await ignorePromiseErrorAsync(responsePromise);
 
 			assert.deepEqual(requests.data, [
 				expectedData,
