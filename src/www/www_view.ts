fc3cff072817ad23f4650e0b38f8da188e58a9c5
James Shore <jshore@jamesshore.com>
2023-02-23 11:59:07 -0800

    Convert WwwView to TypeScript

--- src/www/www_view.ts
+++ src/www/www_view.ts
@@ -7,7 +7,7 @@ import { HttpServerResponse } from "http/http_server_response.js";
  * @param body page body (HTML)
  * @returns {string} HTML for page; still needs to be wrapped in server response
  */
-export function pageTemplate(title, body) {
+export function pageTemplate(title: string, body: string): string {
 	return `
 		<html lang="en">
 		<head>
@@ -24,7 +24,7 @@ export function pageTemplate(title, body) {
  * @param message error description
  * @returns {HttpServerResponse} error page for server to return
  */
-export function errorPage(status, message) {
+export function errorPage(status: number, message: string): HttpServerResponse {
 	const title = `${status}: ${message}`;
 	const body = `<p>${message}</p>`;
 
