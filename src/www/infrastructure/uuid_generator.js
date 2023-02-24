6662086b4c7cacdbd52b5f82def0ca2985589088
James Shore <jshore@jamesshore.com>
2023-02-23 17:16:32 -0800

    JSDoc for UuidGenerator

--- src/www/infrastructure/uuid_generator.js
+++ src/www/infrastructure/uuid_generator.js
@@ -3,26 +3,41 @@ import * as ensure from "util/ensure.js";
 import * as uuid from "uuid";
 import { ConfigurableResponses } from "util/configurable_responses.js";
 
+/** Creates unique IDs. */
 export class UuidGenerator {
 
+	/**
+	 * Factory method.
+	 * @returns {UuidGenerator} the instance
+	 */
 	static create() {
 		ensure.signature(arguments, []);
 
 		return new UuidGenerator(uuid);
 	}
 
+	/**
+	 * Nulled factory method.
+	 * @param [uuids] the UUIDs to return; either a single UUID to return indefinitely, or a list of specific UUIDs
+	 * @returns {UuidGenerator} the nulled instance
+	 */
 	static createNull(uuids = uuid.NIL) {
 		ensure.signature(arguments, [[ undefined, String, Array ]]);
 
 		return new UuidGenerator(new StubbedUuid(uuids));
 	}
 
+	/** @deprecated Use a factory method instead. */
 	constructor(uuid) {
 		ensure.signatureMinimum(arguments, [{ v4: Function }]);
 
 		this._uuid = uuid;
 	}
 
+	/**
+	 * Create a unique ID.
+	 * @returns {string} the ID
+	 */
 	generate() {
 		ensure.signature(arguments, []);
 
