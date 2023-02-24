5e56247463185e233ffdaac75a5067fb32c6673c
James Shore <jshore@jamesshore.com>
2023-02-23 16:51:39 -0800

    JSDoc for WwwView

--- src/www/www_view.js
+++ src/www/www_view.js
@@ -1,7 +1,12 @@
 // Copyright Titanium I.T. LLC.
 import { HttpServerResponse } from "http/http_server_response.js";
 
-/** Overall HTML template for website */
+/**
+ * Overall HTML template for the user-facing website.
+ * @param title page title
+ * @param body page body (HTML)
+ * @returns {string} HTML for page; still needs to be wrapped in server response
+ */
 export function pageTemplate(title, body) {
 	return `
 		<html lang="en">
@@ -13,7 +18,12 @@ export function pageTemplate(title, body) {
 	`;
 }
 
-/** Error page response */
+/**
+ * Error page.
+ * @param status error status code
+ * @param message error description
+ * @returns {HttpServerResponse} error page for server to return
+ */
 export function errorPage(status, message) {
 	const title = `${status}: ${message}`;
 	const body = `<p>${message}</p>`;
