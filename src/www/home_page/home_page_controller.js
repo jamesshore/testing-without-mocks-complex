99e4834341bd30966e318a2feb377b33f4ceef24
James Shore <jshore@jamesshore.com>
2023-02-23 16:54:20 -0800

    JSDoc for HomePageController

--- src/www/home_page/home_page_controller.js
+++ src/www/home_page/home_page_controller.js
@@ -10,19 +10,30 @@ const ENDPOINT = "/";
 const INPUT_FIELD_NAME = "text";
 const TIMEOUT_IN_MS = 5000;
 
-/** Endpoints for / (home page) */
+/** Endpoints for '/' home page. */
 export class HomePageController {
 
+	/**
+	 * Factory method.
+	 * @returns {HomePageController} the instance
+	 */
 	static create() {
 		ensure.signature(arguments, []);
+
 		return new HomePageController(Rot13Client.create(), Clock.create());
 	}
 
+	/**
+	 * Nulled factory method.
+	 * @returns {HomePageController} the nulled instance
+	 */
 	static createNull() {
 		ensure.signature(arguments, []);
+
 		return new HomePageController(Rot13Client.createNull(), Clock.createNull());
 	}
 
+	/** @deprecated Use the factory methods instead. */
 	constructor(rot13Client, clock) {
 		ensure.signature(arguments, [ Rot13Client, Clock ]);
 
@@ -30,12 +41,24 @@ export class HomePageController {
 		this._clock = clock;
 	}
 
+	/**
+	 * Handle GET request.
+	 * @param request HTTP request
+	 * @param config configuration for this request
+	 * @returns {HttpServerResponse} HTTP response
+	 */
 	getAsync(request, config) {
 		ensure.signature(arguments, [ HttpServerRequest, WwwConfig ]);
 
 		return homePageView.homePage();
 	}
 
+	/**
+	 * Handle POST request.
+	 * @param request HTTP request
+	 * @param config configuration for this request
+	 * @returns {Promise<HttpServerResponse>} HTTP response
+	 */
 	async postAsync(request, config) {
 		ensure.signature(arguments, [ HttpServerRequest, WwwConfig ]);
 
