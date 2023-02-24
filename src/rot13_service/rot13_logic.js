cb4dc94f41ed397c35d3075ae0d2802e6965f167
James Shore <jshore@jamesshore.com>
2023-02-23 17:19:42 -0800

    JSDoc for Rot13Logic

--- src/rot13_service/rot13_logic.js
+++ src/rot13_service/rot13_logic.js
@@ -2,6 +2,12 @@
 import * as ensure from "util/ensure.js";
 
 /** ROT-13 transformation logic */
+
+/**
+ * Transform a string using ROT-13 encoding.
+ * @param input the string to transform
+ * @returns {string} the transformed string
+ */
 export function transform(input) {
 	ensure.signature(arguments, [ String ]);
 
