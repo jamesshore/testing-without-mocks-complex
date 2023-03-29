607841daa5bcd12ed77e826b596ca4ed12331c49
James Shore <jshore@jamesshore.com>
2023-03-29 10:41:16 -0700

    Watch script detects when build's event loop isn't drained

--- build/scripts/watch.js
+++ build/scripts/watch.js
@@ -12,6 +12,13 @@ import sound from "sound-play";
 import * as sh from "../util/sh.js";
 import Colors from "../util/colors.js";
 import { pathToFile } from "../util/module_paths.js";
+import child_process from "node:child_process";
+
+const TIMEOUT_IN_MS = 5000;
+const SUCCESS = 0;
+const LINT_ERROR = 1;
+const FAILURE = 2;
+const TIMEOUT = 3;
 
 const watchColor = Colors.cyan;
 const errorColor = Colors.brightRed.inverse;
@@ -66,7 +73,7 @@ async function runBuild() {
 		buildStartedAt = Date.now();
 		console.log(watchColor(`\n\n\n\n*** BUILD> ${args.join(" ")}`));
 
-		const { code } = await shellToBuildAsync(args);
+		const code = await shellToBuildAsync(args);
 		alertBuildResult(code);
 
 		buildRunning = false;
@@ -74,7 +81,28 @@ async function runBuild() {
 }
 
 async function shellToBuildAsync(args) {
-	return await sh.runInteractive("node", [ "--enable-source-maps", buildScript, ...args ]);
+	const command = "node";
+	const commandArgs = [ "--enable-source-maps", buildScript, ...args ];
+
+	const child = child_process.spawn(command, commandArgs, { stdio: "inherit" });
+	const spawnPromise = new Promise((resolve, reject) => {
+		child.on("error", reject);
+		child.on("exit", (code) => { resolve(code); });
+	});
+	const timeoutPromise = new Promise((resolve, reject) => {
+		setTimeout(resolve.bind(null, TIMEOUT), TIMEOUT_IN_MS);
+	});
+
+	const code = await Promise.race([ spawnPromise, timeoutPromise ]);
+	if (code === TIMEOUT) {
+		console.log(errorColor("  BUILD TIMED OUT  "));
+		child.kill();
+		await new Promise((resolve, reject) => {
+			child.on("exit", resolve);
+		});
+	}
+
+	return code;
 }
 
 function queueAnotherBuild() {
@@ -95,9 +123,11 @@ function alertBuildResult(exitCode) {
 
 	function pathForCode(exitCode) {
 		switch (exitCode) {
-			case 0: return "../sounds/success.wav";
-			case 1: return "../sounds/lint_error.wav";
-			case 2: return "../sounds/fail.wav";
+			case SUCCESS: return "../sounds/success.wav";
+			case LINT_ERROR: return "../sounds/lint_error.wav";
+			case FAILURE:
+			case TIMEOUT:
+				return "../sounds/fail.wav";
 			default: throw new Error(`Unrecognized exit code from build: ${exitCode}`);
 		}
 	}
